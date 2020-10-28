942194cd2217e18fda832df2430d7ea15fea828f
JaganKaartik <kaartikjagan@gmail.com>
2020-10-28 21:37:07 +0530

    Added Listen

--- app/index.ts
+++ app/index.ts
@@ -5,9 +5,15 @@ const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const port = process.env.PORT || 5000
+
 const mongoDB = process.env.mongoDB_URL || ''
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use('/routes', routes)
+
+app.listen(port, () => {
+  console.log('Server Started Successfully')
+})
