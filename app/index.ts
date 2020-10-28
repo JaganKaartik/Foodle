b9557538da752df8f20dae2b8819d4aedf361678
JaganKaartik <kaartikjagan@gmail.com>
2020-10-28 21:09:18 +0530

    Mongo Db Connection

--- app/index.ts
+++ app/index.ts
@@ -2,5 +2,12 @@ import express from 'express'
 
 const routes = './routes'
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const mongoDB = process.env.mongoDB_URL || ''
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use('/routes', routes)
