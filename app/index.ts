00fbde9b326c8096c51b8d2d59a68cf5320149ef
JaganKaartik <kaartikjagan@gmail.com>
2020-10-28 23:18:25 +0530

    Added DB URL

--- app/index.ts
+++ app/index.ts
@@ -7,7 +7,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const routes = require('./routes/api')
 
-const mongoDB = process.env.mongoDB_URL || 'mongodb://127.0.0.1/foodle'
+const mongoDB = process.env.mongoDB_URL || 'mongodb://127.0.0.1:27017/foodle'
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
