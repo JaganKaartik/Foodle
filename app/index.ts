0ac4b7c2a0f48d7e4c5ba352eca4fc67de450645
JaganKaartik <kaartikjagan@gmail.com>
2020-10-28 22:41:33 +0530

    Added Routes + Lint Fixes

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 
-const routes = './routes'
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
+const routes = require('./routes/api')
 
 const mongoDB = process.env.mongoDB_URL || 'mongodb://127.0.0.1/foodle'
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
