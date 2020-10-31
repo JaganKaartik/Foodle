f15f48302db459351640c7279985053065bdd386
JaganKaartik <kaartikjagan@gmail.com>
2020-10-31 15:46:52 +0530

    Minor

--- app/config/database.ts
+++ app/config/database.ts
@@ -15,7 +15,6 @@ const connectDB = async () => {
     console.log('MongoDB Connected...')
   } catch (err) {
     console.error(err.message)
-    // Exit process with failure
     process.exit(1)
   }
 }
