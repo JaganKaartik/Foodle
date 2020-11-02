e6d77aff3a2b087ecd5b0b3b70196dceae9b0ecc
JaganKaartik <kaartikjagan@gmail.com>
2020-11-02 20:49:55 +0530

    Added Get All Dish

---
+++ app/controllers/model/dish.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+const DishSchema = require('../../models')
+
+const getAllDish = (req, res) => {
+  DishSchema.find({})
+    .then((data: JSON) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+export = getAllDish
