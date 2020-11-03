564f2e1e434e01634aa9b293d297cc0d98a05f5e
JaganKaartik <kaartikjagan@gmail.com>
2020-11-03 22:52:40 +0530

    Root Controller

--- app/controllers/common/index.ts
+++ app/controllers/common/index.ts
@@ -1,3 +1,7 @@
+const rootController = (req, res) => {
+  res.send('<h1>Welcome to Foodle API Root</h1>')
+}
+
 const homeController = (req, res) => {
   res.send('<h1>Welcome to Home</h1>')
 }
@@ -6,4 +10,4 @@ const errorController = (req, res) => {
   res.send('<h1>Error Page</h1>')
 }
 
-export { homeController, errorController }
+export { rootController, homeController, errorController }
--- app/routes/common/index.ts
+++ app/routes/common/index.ts
@@ -1,8 +1,13 @@
 import express from 'express'
-import { homeController, errorController } from '../../controllers/common'
+import {
+  rootController,
+  homeController,
+  errorController
+} from '../../controllers/common'
 
 const CommonRouter = express.Router()
 
+CommonRouter.get('/', rootController)
 CommonRouter.get('/home', homeController)
 CommonRouter.get('/error', errorController)
 
