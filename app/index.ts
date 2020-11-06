9d5fbe4dc2f15e4977006ea1abbe81b6007ea768
JaganKaartik <kaartikjagan@gmail.com>
2020-11-07 00:01:08 +0530

    Shifted Body-Parser

--- app/index.ts
+++ app/index.ts
@@ -3,11 +3,8 @@ import { connectDB } from './config/database'
 import Middleware from './middleware'
 
 const app = express()
-const bodyParser = require('body-parser')
 
 connectDB()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 app.use(Middleware)
 
 const port = process.env.PORT || 4000
