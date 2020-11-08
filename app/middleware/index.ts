54d85d33a9820fa22beb2c425fb515a4bd3baac3
JaganKaartik <kaartikjagan@gmail.com>
2020-11-08 16:46:41 +0530

    Added CORS

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -9,6 +9,7 @@ const session = require('express-session')
 const cookieSession = require('cookie-session')
 const cookieParser = require('cookie-parser')
 const passport = require('passport')
+const cors = require('cors')
 const passportSetup = require('../config/passport-setup')
 
 Middleware.use(
@@ -27,6 +28,15 @@ Middleware.use(passport.initialize())
 
 // Deserialize cookie from the browser
 Middleware.use(passport.session())
+
+// Cors
+Middleware.use(
+  cors({
+    origin: process.env.CLIENT_HOME_PAGE_URL || '', // allow to server to accept request from different origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true // allow session cookie from browser to pass through
+  })
+)
 
 Middleware.use(bodyParser.urlencoded({ extended: false }))
 Middleware.use(bodyParser.json())
