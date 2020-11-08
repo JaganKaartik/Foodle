651cc073a3c2b85571c1c3334b7665a8ce6e5522
JaganKaartik <kaartikjagan@gmail.com>
2020-11-08 16:33:13 +0530

    Added Auth Middlwares

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -1,13 +1,60 @@
 import express from 'express'
 import { apiRouter, authRouter, commonRouter } from '../routes'
 
+require('dotenv').config()
+
 const Middleware = express()
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const cookieSession = require('cookie-session')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const passportSetup = require('../config/passport-setup')
+
+Middleware.use(
+  cookieSession({
+    name: 'session',
+    keys: [process.env.COOKIE_KEY],
+    maxAge: 24 * 60 * 60 * 100
+  })
+)
+
+// Parse cookies
+Middleware.use(cookieParser())
+
+// Initalize passport
+Middleware.use(passport.initialize())
+
+// Deserialize cookie from the browser
+Middleware.use(passport.session())
 
 Middleware.use(bodyParser.urlencoded({ extended: false }))
 Middleware.use(bodyParser.json())
 Middleware.use(commonRouter)
 Middleware.use('/api', apiRouter)
 Middleware.use('/auth', authRouter)
+
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.status(401).json({
+      authenticated: false,
+      message: 'user has not been authenticated'
+    })
+  } else {
+    next()
+  }
+}
+
+// if it's already login, send the profile response,
+// otherwise, send a 401 response that the user is not authenticated
+// authCheck before navigating to home page
+Middleware.get('/', authCheck, (req, res) => {
+  res.status(200).json({
+    authenticated: true,
+    message: 'user successfully authenticated',
+    user: req.user,
+    cookies: req.cookies
+  })
+})
 
 export = Middleware
