a128dd3917675ae24c45d9ddc1ca44ad41218d8d
JaganKaartik <kaartikjagan@gmail.com>
2020-11-09 18:01:14 +0530

    Improved Readability

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -2,14 +2,15 @@ import express from 'express'
 import { auth, authStatus, authRedirect, logout } from '../controllers/auth'
 
 const authRouter = express.Router()
-// when login is successful, retrieve user info
-authRouter.get('/status', authStatus)
 
 // When logout, redirect to client
 authRouter.get('/logout', logout)
 
 // auth with twitter
 authRouter.get('/twitter', auth)
+
+// when login is successful, retrieve user info
+authRouter.get('/status', authStatus)
 
 // redirect to home page after successfully login via twitter
 authRouter.get('/twitter/redirect', authRedirect)
