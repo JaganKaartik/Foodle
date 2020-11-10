484f6bfe0f2c532eab4810bf2638df80bea1fa8e
JaganKaartik <kaartikjagan@gmail.com>
2020-11-10 17:18:54 +0530

    minor

--- app/config/passport-setup.ts
+++ app/config/passport-setup.ts
@@ -5,23 +5,6 @@ const User = require('../models/user')
 
 require('dotenv').config()
 
-// serialize the user.id to save in the cookie session
-// so the browser will remember the user when login
-passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
-
-// deserialize the cookieUserId to user in the database
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user)
-    })
-    .catch((e) => {
-      done(new Error('Failed to deserialize an user'))
-    })
-})
-
 passport.use(
   new TwitterStrategy(
     {
@@ -50,3 +33,20 @@ passport.use(
     }
   )
 )
+
+// serialize the user.id to save in the cookie session
+// so the browser will remember the user when login
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+// deserialize the cookieUserId to user in the database
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user)
+    })
+    .catch((e) => {
+      done(new Error('Failed to deserialize an user'))
+    })
+})
