cb48e726596fd7b4fc8bf5a8c5210ed28247ae85
JaganKaartik <kaartikjagan@gmail.com>
2020-11-11 19:29:34 +0530

    API Tests -- UPDATE WORKING

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -72,8 +72,8 @@ export const delDish = (id) => {
   });
 };
 
-export const updateDish = (id, name, price) => {
-  return fetch(server_url + `/api/dish/${id}`, {
+export const updateDish = (name, price) => {
+  return fetch(server_url + `/api/dish/`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
