0ee5bfbd6356abd3cb74079e203b61d3e8dcf4e4
JaganKaartik <kaartikjagan@gmail.com>
2020-11-11 11:51:15 +0530

    minor

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -3,16 +3,9 @@ import { auth, authRedirect, authStatus, logout } from '../controllers/auth'
 
 const authRouter = express.Router()
 
-// auth with twitter
 authRouter.get('/twitter', auth)
-
-// when login is successful, retrieve user info
-authRouter.get('/status', authStatus)
-
-// redirect to home page after successfully login via twitter
 authRouter.get('/twitter/redirect', authRedirect)
-
-// When logout, redirect to client
+authRouter.get('/status', authStatus)
 authRouter.get('/logout', logout)
 
 export = authRouter
