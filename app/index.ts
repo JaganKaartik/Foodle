5dc650114b82d6c83cdf849a9a27316e989c2fe0
JaganKaartik <kaartikjagan@gmail.com>
2020-11-14 22:22:27 +0530

    Added Seed

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,20 @@
 import express from 'express'
 import { connectDB } from './config/database'
 import Middleware from './middleware'
+import seed from './config/seed'
+
+require('dotenv').config()
 
 const app = express()
 
 connectDB()
 app.use(Middleware)
 
-const port = process.env.PORT || 4000
+if (process.env.NODE_ENV === 'prod-local') {
+  seed()
+}
+
+const port = process.env.PORT || 7000
 
 app.listen(port, () => {
   console.log('Server Started Successfully')
