be84cf19e78b119d64b6c198d0a0824d0b119a87
JaganKaartik <kaartikjagan@gmail.com>
2020-11-14 23:44:12 +0530

    Updated Seed Script

--- app/config/seed.ts
+++ app/config/seed.ts
@@ -3,6 +3,10 @@ import Dish from '../models/dish'
 
 const seed = async () => {
   await Dish.insertMany(seedData)
+    .then((res) => {
+      console.log(`Successfully inserted ${res.insertedIds.length} items!`)
+    })
+    .catch((err) => console.error(`Failed to insert documents: ${err}`))
 }
 
 export = seed
