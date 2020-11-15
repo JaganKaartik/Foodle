6492fe4666140ebc1007281e545d792931dde27f
JaganKaartik <kaartikjagan@gmail.com>
2020-11-16 00:21:06 +0530

    Added Check Validation on routes

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -8,12 +8,31 @@ import {
 } from '../controllers/api'
 import authCheck from '../middleware/auth-check'
 
+const { check } = require('express-validator')
+
 const apiRouter = express.Router()
 
 apiRouter.get('/dish/all', authCheck, getAllDish)
-apiRouter.get('/dish/:id', authCheck, getDish)
-apiRouter.delete('/dish/:id', authCheck, deleteDish)
-apiRouter.put('/dish/', authCheck, updateDish)
-apiRouter.post('/dish/', authCheck, addDish)
+apiRouter.get('/dish/:id', authCheck, [check('id').exists()], getDish)
+apiRouter.delete('/dish/:id', authCheck, [check('id').exists()], deleteDish)
+apiRouter.put(
+  '/dish/',
+  authCheck,
+  [
+    check('name').exists().isString().notEmpty(),
+    check('price').exists().isString().notEmpty()
+  ],
+  updateDish
+)
+apiRouter.post(
+  '/dish/',
+  authCheck,
+  [
+    check('name').exists().isString().notEmpty(),
+    check('type').exists().isString().notEmpty(),
+    check('price').exists().isString().notEmpty()
+  ],
+  addDish
+)
 
 export = apiRouter
