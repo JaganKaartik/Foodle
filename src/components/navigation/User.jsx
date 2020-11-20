b48f82f35122aa59ccd746e9b50d9ab7b3ec1e4d
JaganKaartik <kaartikjagan@gmail.com>
2020-11-20 18:51:36 +0530

    Updated User Profile Component

--- src/components/navigation/User.jsx
+++ src/components/navigation/User.jsx
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import '../../static/css/home.css';
 export default class User extends Component {
   render() {
+    const authProvider = this.props.userInfo.provider;
     return (
       <div>
         <div className="container-login100">
           <div className="wrap-login100">
-            <img
-              src={this.props.userInfo.profileImageUrl}
-              alt="no-img"
-              class="inner-image-profile"
-            />
-            <img src={this.props.userInfo.profileBannerUrl} alt="no-img" />
-            <br />
-            <span style={{ display: 'inline-block', width: 100 }}></span>
-            <span class="twitter-profile">{this.props.userInfo.name}</span>
+            {authProvider === 'twitter' ? (
+              <div>
+                <img
+                  src={this.props.userInfo.profileImageUrl}
+                  alt="no-img"
+                  class="inner-image-profile"
+                />
+                <img src={this.props.userInfo.profileBannerUrl} alt="no-img" />
+                <br />
+                <span style={{ display: 'inline-block', width: 100 }}></span>
+                <span class="twitter-profile">{this.props.userInfo.name}</span>
+              </div>
+            ) : (
+              <div>
+                <img src={this.props.userInfo.profileImageUrl} alt="no-img" />
+                <span class="twitter-profile">{this.props.userInfo.name}</span>
+              </div>
+            )}
           </div>
         </div>
       </div>
