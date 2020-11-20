74a0a7fcad1bb3f2454a96dccfd56913062e10d4
JaganKaartik <kaartikjagan@gmail.com>
2020-11-20 22:52:12 +0530

    Minot

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -3,4 +3,4 @@ export { default as Navigation } from './Navigation';
 export { default as PrivateRoute } from './PrivateRoute';
 export { default as AuthHomeRoute } from './AuthHomeRoute';
 export { default as User } from './User';
-export { default as SearchDish } from './SearchDish';
+export { default as SearchDish } from './searchDish';
