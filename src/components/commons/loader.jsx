c6d7209bd85e464ef7312923ca2e0bbf71249978
JaganKaartik <kaartikjagan@gmail.com>
2020-11-21 00:26:25 +0530

    Removed Unwanted Dependencies, Removed Atlaskit Loader

--- src/components/commons/loader.jsx
+++ src/components/commons/loader.jsx
@@ -1,43 +1,13 @@
 import React, { Component } from 'react';
-import { ProgressIndicator } from '@atlaskit/progress-indicator';
-import styled from 'styled-components';
 
-const Footer = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  padding: 1em;
-`;
-class FoodleLoader extends Component {
-  static defaultProps = {
-    selectedIndex: 0,
-    values: ['first', 'second', 'third']
-  };
-
-  state = {
-    selectedIndex: this.props.selectedIndex
-  };
-
-  handlePrev = () => {
-    this.setState((prevState) => ({
-      selectedIndex: prevState.selectedIndex - 1
-    }));
-  };
-
-  handleNext = () => {
-    this.setState((prevState) => ({
-      selectedIndex: prevState.selectedIndex + 1
-    }));
-  };
-
+export default class FoodleLoader extends Component {
   render() {
-    const { values } = this.props;
-    const { selectedIndex } = this.state;
     return (
-      <Footer style={{ backgroundColor: '#1b2638' }}>
-        <ProgressIndicator selectedIndex={selectedIndex} values={values} appearance={'inverted'} />
-      </Footer>
+      <div>
+        <div class="progress">
+          <div class="indeterminate"></div>
+        </div>
+      </div>
     );
   }
 }
-export default FoodleLoader;
