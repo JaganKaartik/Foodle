b78e5c28a495e82fb854e01d622f91747a2c064c
JaganKaartik <kaartikjagan@gmail.com>
2020-11-20 13:46:22 +0530

    Updated Rate Limit

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -21,7 +21,7 @@ Middleware.use(
 
 const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 5,
+  max: 25,
   message:
     'Too many accounts created from this IP, please try again after a minute'
 })
