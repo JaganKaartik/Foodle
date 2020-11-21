f9787fac272311ed5b17dab5ab6a040e7b9a42f1
JaganKaartik <kaartikjagan@gmail.com>
2020-11-21 18:04:16 +0530

    Updated CSS

---
+++ build/precache-manifest.36379de7e372182e4aa34ad357a6610c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c1cf0dfaa76de5f749367dbc68d1d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fb6621511fde715a395",
+    "url": "/static/css/main.ae23479b.chunk.css"
+  },
+  {
+    "revision": "3f4be75837ad2ed7eb67",
+    "url": "/static/js/2.3eef58a4.chunk.js"
+  },
+  {
+    "revision": "5a1768750c09c8fadbc94e74a41bcc3a",
+    "url": "/static/js/2.3eef58a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb6621511fde715a395",
+    "url": "/static/js/main.51ff6750.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  },
+  {
+    "revision": "f6bbd3b60eafb8526e60a6b322ee6068",
+    "url": "/static/media/bg-main.f6bbd3b6.jpg"
+  },
+  {
+    "revision": "7954fc3e0216cfe18cd26e0ae03875db",
+    "url": "/static/media/home-L.7954fc3e.jpg"
+  },
+  {
+    "revision": "e92fb50a5c632688e29d8db237990121",
+    "url": "/static/media/home-R.e92fb50a.jpg"
+  }
+]);
