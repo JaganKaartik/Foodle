59de4f5cbb69375d53598175f17c48863e0dcfc0
JaganKaartik <kaartikjagan@gmail.com>
2021-03-23 18:09:35 +0530

    Removed MaterializeCSS. Updated Nav-bar'

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -9,16 +9,12 @@ class Navigation extends Component {
   render() {
     return (
       <div>
-        <header className="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
-          <div className="flex-1 flex justify-between items-center">
+        <header className="bg-blue-50 lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
+          <div className="flex-1 flex">
             <NavLink to="/">
-              <img src={logoSVG} width="32" height="36" alt="Andy Leverenz" />
-              {/* <svg width="32" height="36" viewBox="0 0 32 36" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.922 35.798c-.946 0-1.852-.228-2.549-.638l-10.825-6.379c-1.428-.843-2.549-2.82-2.549-4.501v-12.762c0-1.681 1.12-3.663 2.549-4.501l10.825-6.379c.696-.41 1.602-.638 2.549-.638.946 0 1.852.228 2.549.638l10.825 6.379c1.428.843 2.549 2.82 2.549 4.501v12.762c0 1.681-1.12 3.663-2.549 4.501l-10.825 6.379c-.696.41-1.602.638-2.549.638zm0-33.474c-.545 0-1.058.118-1.406.323l-10.825 6.383c-.737.433-1.406 1.617-1.406 2.488v12.762c0 .866.67 2.05 1.406 2.488l10.825 6.379c.348.205.862.323 1.406.323.545 0 1.058-.118 1.406-.323l10.825-6.383c.737-.433 1.406-1.617 1.406-2.488v-12.757c0-.866-.67-2.05-1.406-2.488l-10.825-6.379c-.348-.21-.862-.328-1.406-.328zM26.024 13.104l-7.205 13.258-3.053-5.777-3.071 5.777-7.187-13.258h4.343l2.803 5.189 3.107-5.832 3.089 5.832 2.821-5.189h4.352z"></path>
-              </svg> */}
+              <img src={logoSVG} width="32" heigsht="36" alt="Andy Leverenz" />
             </NavLink>
           </div>
-
           <label for="menu-toggle" className="pointer-cursor lg:hidden block">
             <svg
               className="fill-current text-gray-900"
@@ -32,7 +28,6 @@ class Navigation extends Component {
             </svg>
           </label>
           <input className="hidden" type="checkbox" id="menu-toggle" />
-
           <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
             <nav>{this.displayNav()}</nav>
             {this.displayUser()}
@@ -44,7 +39,7 @@ class Navigation extends Component {
   displayNav() {
     if (this.props.authstate === true) {
       return (
-        <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+        <ul className="nav-text lg:flex items-center justify-between text-base text-blue-400 pt-4 lg:pt-0">
           <li>
             <div className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2">
               <NavLink to="/dashboard">Dashboard</NavLink>
@@ -76,7 +71,7 @@ class Navigation extends Component {
       );
     } else {
       return (
-        <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+        <ul className="nav-text lg:flex items-center justify-between text-base text-indigo-700 pt-4 lg:pt-0">
           <li>
             <div className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2">
               <NavLink to="/login">Login</NavLink>
