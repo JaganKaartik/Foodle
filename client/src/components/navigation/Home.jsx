e403b55a042bbae2949faabaeea25de8e248065d
JaganKaartik <kaartikjagan@gmail.com>
2021-03-26 16:22:00 +0530

    Updated UI. Dark Mode (Toggle-Rem)

--- client/src/components/navigation/Home.jsx
+++ client/src/components/navigation/Home.jsx
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import bg1 from '../../assets/images/Cooking_Dark.gif';
 import bg2 from '../../assets/images/SI_Dark.gif';
-class Home extends Component {
-  render() {
-    return (
-      <div className="home-grid flex-grow home-container">
-        {/* <div className="col-start-1 row-start-1"> */}
-        <div>
-          <img className="gifs" src={bg2} alt="cant load img" />
-        </div>
 
-        {/* <div className="p-2 col-start-2 row-start-1"> */}
-        <div>
-          <div className="home-card shadow-2xl max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-            <div className="md:flex">
-              <div className="md:flex-shrink-0"></div>
-              <div className="p-10">
-                <div className="home-content block mt-1 text-6xl text-blue-500 leading-tight">
-                  Foodle is a SPA built on the MERN stack, a CRUD application dealing with Dishes.
-                </div>
+function Home() {
+  return (
+    <div className="home-grid flex-grow home-container">
+      {/* <div className="col-start-1 row-start-1"> */}
+      <div>
+        <img className="gifs" src={bg2} alt="cant load img" />
+      </div>
+
+      {/* <div className="p-2 col-start-2 row-start-1"> */}
+      <div>
+        <div className="home-card shadow-2xl max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div className="md:flex">
+            <div className="md:flex-shrink-0"></div>
+            <div className="p-10">
+              <div className="home-content block mt-1 text-blue-500 leading-tight">
+                Foodle is a SPA built on the MERN stack, a CRUD application dealing with Dishes.
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* <div className="col-start-2 row-start-2"> */}
-        <div>
-          <img className="gifs" src={bg1} alt="cant load img" />
-        </div>
+      {/* <div className="col-start-2 row-start-2"> */}
+      <div>
+        <img className="gifs" src={bg1} alt="cant load img" />
+      </div>
 
-        {/* <div className="p-2 col-start-2 row-start-1"> */}
-        {/* <div>
+      {/* <div className="p-2 col-start-2 row-start-1"> */}
+      {/* <div>
           <div className="home-card shadow-2xl max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
             <div className="md:flex">
               <div className="md:flex-shrink-0"></div>
@@ -43,31 +43,31 @@ class Home extends Component {
           </div>
         </div> */}
 
-        {/* <div className="col-start-1 row-start-2 "> */}
-        <div>
-          <div className="home-card shadow-2xl max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-            <div className="md:flex">
-              <div className="md:flex-shrink-0"></div>
-              <div className="p-10">
-                <div className="block mt-1 text-lg leading-tight font-medium text-blue-800 hover:underline">
-                  Tech Stack & Features
-                </div>
-                <p className="mt-2 text-blue-500">
-                  Built using React (with Context API & Hooks), Redux, TailwindCSS, Atlaskit.
-                </p>
-                <p className="mt-2 text-blue-500">
-                  Server built using Typescript, Express.js-Node.js & MongoDB as the database.
-                </p>
-                <p className="mt-2 text-blue-500">
-                  Features OAuth Authentication (Google & Twitter), JWT Session, API validation,
-                  Dockerized client & server for containerized deployment.
-                </p>
+      {/* <div className="col-start-1 row-start-2 "> */}
+      <div>
+        <div className="home-card shadow-2xl max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div className="md:flex">
+            <div className="md:flex-shrink-0"></div>
+            <div className="p-10">
+              <div className="block mt-1 text-lg leading-tight font-medium text-blue-800 hover:underline">
+                Tech Stack & Features
               </div>
+              <p className="mt-2 text-blue-500">
+                Built using React (with Context API & Hooks), Redux, TailwindCSS, Atlaskit.
+              </p>
+              <p className="mt-2 text-blue-500">
+                Server built using Typescript, Express.js-Node.js & MongoDB as the database.
+              </p>
+              <p className="mt-2 text-blue-500">
+                Features OAuth Authentication (Google & Twitter), JWT Session, API validation,
+                Dockerized client & server for containerized deployment.
+              </p>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* <div className="col-start-1 row-start-1">
+      {/* <div className="col-start-1 row-start-1">
         <div>
           <img className="gifs" src={bg2} alt="cant load img" />
         </div>
@@ -76,9 +76,8 @@ class Home extends Component {
         <div>
           <img className="gifs" src={bg1} alt="cant load img" />
         </div> */}
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
