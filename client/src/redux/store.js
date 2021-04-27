5a1986a67c059dbda56f17c888d90e5a43ce251f
JaganKaartik <kaartikjagan@gmail.com>
2021-04-27 21:35:52 +0530

    Minor. Prod Redux Update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const devTools =
+  process.env.NODE_ENV === "production"
+    ? applyMiddleware(thunk)
+    : composeWithDevTools(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, devTools);
 
 export default store;
